47b5b092a02f0afd00f93e317315026acba49a83
Francisco Jose Ben Ramírez <96696655+ElNeko42@users.noreply.github.com>
2022-06-06 12:20:39 +0200

    Update Anunciante.java

--- marcador/controlador/Proyecto_ev3/src/proyecto_ev3/Anunciante.java
+++ marcador/controlador/Proyecto_ev3/src/proyecto_ev3/Anunciante.java
@@ -4,11 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *  Clase Anunciante
- * 
- *  @author angel
- */
+
+
 public class Anunciante extends ConnDB {
 
     // Atributos
