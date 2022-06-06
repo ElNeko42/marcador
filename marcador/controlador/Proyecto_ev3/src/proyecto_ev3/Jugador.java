e51b6bc3757a97239b02153b3156bb8ab28d5850
Francisco Jose Ben Ramírez <96696655+ElNeko42@users.noreply.github.com>
2022-06-06 12:22:19 +0200

    Update Jugador.java

--- marcador/controlador/Proyecto_ev3/src/proyecto_ev3/Jugador.java
+++ marcador/controlador/Proyecto_ev3/src/proyecto_ev3/Jugador.java
@@ -4,11 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *  Clase Jugador
- * 
- *  @author angel
- */
+
 public class Jugador extends ConnDB {
     
     // Atributos
