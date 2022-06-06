333ba623a628ba587ddf05ad940f429f2d40696f
Francisco Jose Ben Ramírez <96696655+ElNeko42@users.noreply.github.com>
2022-06-06 12:21:02 +0200

    Update ConnDB.java

--- marcador/controlador/Proyecto_ev3/src/proyecto_ev3/ConnDB.java
+++ marcador/controlador/Proyecto_ev3/src/proyecto_ev3/ConnDB.java
@@ -4,11 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *  Clase principal para conectarse a la base de datos
- *
- *  @author angel
- */
+
+
 public class ConnDB {
 
     // Atributos
