7f9130d45bcea62c56868cf0d45ae7e689da922d
Francisco Jose Ben Ramírez <96696655+ElNeko42@users.noreply.github.com>
2022-06-06 16:13:58 +0200

    Update TorneoEquipo.java

--- marcador/controlador/Proyecto_ev3/src/proyecto_ev3/TorneoEquipo.java
+++ marcador/controlador/Proyecto_ev3/src/proyecto_ev3/TorneoEquipo.java
@@ -4,11 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *  Clase TorneoEquipo
- * 
- *  @author angel
- */
+
 public class TorneoEquipo extends ConnDB {
     
     // Atributos
